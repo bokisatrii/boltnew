3480be38ff0501da7ed733ae4cfa846238afb494
bokisatrii <boki.t2002.12@gmail.com>
2026-03-10 15:56:50 +0100

    Updated blog.js

--- netlify/functions/blog.js
+++ netlify/functions/blog.js
@@ -1,17 +1,31 @@
-// netlify/functions/blog.js
-const fetch = require('node-fetch');
+const https = require('https');
 
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwF3no5_3qdGcyaVzC_5jVcGNHESD8yLGLyKRvpYbt4XtJgV95ODDwGlqNb3abZPpjj/exec';
 
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      // Prati redirecte (Google Apps Script uvijek redirectuje)
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        return fetchUrl(res.headers.location).then(resolve).catch(reject);
+      }
+
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(data));
+        } catch (e) {
+          reject(new Error('Invalid JSON: ' + data.substring(0, 200)));
+        }
+      });
+    }).on('error', reject);
+  });
+}
+
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch(APPS_SCRIPT_URL, {
-      method: 'GET',
-      headers: { 'Accept': 'application/json' },
-      redirect: 'follow'
-    });
-
-    const data = await response.json();
+    const data = await fetchUrl(APPS_SCRIPT_URL);
 
     return {
       statusCode: 200,
